d405332bbfccf5f5b57f8901af2871e483f17034
Prathamesh Pande <pandepm28@gmail.com>
2023-10-03 21:21:32 +0530

    form updated

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const SignUp = () => {
+
+
+  
   return (
     <div className="w-2/4 mx-auto my-6">
       <h2 class=" font-serif font-semibold text-3xl mb-10 text-center">Sign Up</h2>
@@ -69,22 +72,6 @@ const SignUp = () => {
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Password
-          </label>
-        </div>
-        <div className="relative z-0 w-full mb-6 group">
-          <input
-            type="password"
-            name="cpassword"
-            id="password"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required=""
-          />
-          <label
-            htmlFor="password"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Confirm password
           </label>
         </div>
         
